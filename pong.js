1d7183565d6b498fc4d5fa173e05cd2fe58a1967
Charles Brandt <luftbuefel@users.noreply.github.com>
2019-02-14 17:02:33 -0800

    Update pong.js
    hooking the gameplay back up after adding networking

--- pong.js
+++ pong.js
@@ -1,9 +1,10 @@
+let paddleRightPosition = 0
 let ballXSpeed = 0
-let lastTimeChecked = 0
 let paddleB: game.LedSprite = null
-let ballMoveTimer = 0
 let ball: game.LedSprite = null
 let paddleA: game.LedSprite = null
+let ballMoveTimer = 0
+let lastTimeChecked = 0
 let IStart = 0
 let gameIsPlaying = false
 input.onButtonPressed(Button.AB, function () {
@@ -45,30 +46,47 @@ radio.onReceivedValue(function (name, value) {
         startBall()
     }
     if (name == "youWin") {
-        gameIsPlaying = false
-        basic.showIcon(IconNames.Happy)
-        music.beginMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
-        game.gameOver()
+        game.addScore(1)
+        IWin()
     }
 })
 function youWin() {
     radio.sendString("youWin")
     game.removeLife(1)
+    basic.showIcon(IconNames.Skull)
 }
 function passBall() {
     radio.sendValue("pass", ball.get(LedSpriteProperty.X))
+}
+function playIdle() {
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Square)
+}
+function IWin() {
+    gameIsPlaying = false
+    basic.showIcon(IconNames.Happy)
+    music.beginMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+}
+function updateBall() {
+    if (input.runningTime() - lastTimeChecked >= ballMoveTimer) {
+        lastTimeChecked = input.runningTime()
+        ball.move(1)
+    }
 }
 radio.setGroup(1)
 radio.setTransmitPower(7)
 ballMoveTimer = 0
 lastTimeChecked = 0
 ballXSpeed = 0
+paddleRightPosition = 2
+ball = game.createSprite(2, 2)
+ball.turn(Direction.Right, 90)
+paddleA = game.createSprite(2, 4)
+paddleB = game.createSprite(3, 4)
 basic.forever(function () {
     if (gameIsPlaying) {
-        basic.showIcon(IconNames.Heart)
-        basic.showIcon(IconNames.SmallHeart)
+        updateBall()
     } else {
-        basic.showIcon(IconNames.SmallSquare)
-        basic.showIcon(IconNames.Square)
+        playIdle()
     }
 })
