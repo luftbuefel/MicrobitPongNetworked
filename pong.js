32d0b301209c2e0ba491796c8c4924db815a9c65
Charles Brandt <luftbuefel@users.noreply.github.com>
2019-02-14 16:51:27 -0800

    Update pong.js
    wrote all the networking communication code

--- pong.js
+++ pong.js
@@ -1,31 +1,28 @@
 let ballXSpeed = 0
-let paddleB: game.LedSprite = null
 let lastTimeChecked = 0
-let paddleA: game.LedSprite = null
+let paddleB: game.LedSprite = null
 let ballMoveTimer = 0
 let ball: game.LedSprite = null
+let paddleA: game.LedSprite = null
 let IStart = 0
 let gameIsPlaying = false
-function youWin() {
-    radio.sendString("youWin")
-    game.removeLife(1)
-}
+input.onButtonPressed(Button.AB, function () {
+    StartMatch()
+})
 input.onButtonPressed(Button.A, function () {
     if (paddleA.get(LedSpriteProperty.X) > 0) {
         paddleA.change(LedSpriteProperty.X, -1)
         paddleB.change(LedSpriteProperty.X, -1)
     }
 })
-function passBall() {
-    radio.sendValue("pass", ball.get(LedSpriteProperty.X))
-}
 input.onButtonPressed(Button.B, function () {
     if (paddleA.get(LedSpriteProperty.X) < 3) {
         paddleA.change(LedSpriteProperty.X, 1)
         paddleB.change(LedSpriteProperty.X, 1)
     }
 })
-function IStart2() {
+function StartMatch() {
+    gameIsPlaying = false
     IStart = Math.randomRange(0, 1)
     radio.sendValue("IStart", IStart)
     if (IStart == 1) {
@@ -33,33 +30,45 @@ function IStart2() {
     }
 }
 function startBall() {
+    music.beginMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
     gameIsPlaying = true
-    basic.showIcon(IconNames.Heart)
 }
 radio.onReceivedValue(function (name, value) {
     if (name == "IStart") {
         if (value == 0) {
             startBall()
         } else {
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
             gameIsPlaying = false
         }
-    } else {
-    	
+    }
+    if (name == "pass") {
+        startBall()
+    }
+    if (name == "youWin") {
+        gameIsPlaying = false
+        basic.showIcon(IconNames.Happy)
+        music.beginMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
+        game.gameOver()
     }
 })
-gameIsPlaying = false
+function youWin() {
+    radio.sendString("youWin")
+    game.removeLife(1)
+}
+function passBall() {
+    radio.sendValue("pass", ball.get(LedSpriteProperty.X))
+}
 radio.setGroup(1)
 radio.setTransmitPower(7)
-IStart2()
+ballMoveTimer = 0
+lastTimeChecked = 0
+ballXSpeed = 0
 basic.forever(function () {
     if (gameIsPlaying) {
-    	
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    } else {
+        basic.showIcon(IconNames.SmallSquare)
+        basic.showIcon(IconNames.Square)
     }
 })
